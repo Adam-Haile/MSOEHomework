5d1e3b7cd78a11d5d0073dc5e43e444ca3e471a4
hailea <hailea@msoe.edu>
2022-10-27 15:32:17 -0500

    Added Week 8 Homework - Elevator & ElevatorDriver

---
+++ src/weekeight/Elevator.java
@@ -0,0 +1,62 @@
+/*
+ * Course: CS1011 - Saikath Bhattacharya
+ * Fall 2022
+ * Homework 8 - Elevator
+ * Name: Adam Haile
+ * Created: 10/27/22
+ */
+
+package weekeight;
+import java.util.Scanner;
+
+/**
+ * The Elevator class - Stores data for ElevatorDriver
+ */
+public class Elevator {
+
+    int currentFloor = 1;
+    static final int MAX_FLOOR = 100;
+
+    /**
+     * Changes the current floor of the user in the elevator
+     * @param in Scanner: Takes in the floor inputted from ElevatorDriver
+     */
+    public void setFloor(Scanner in) {
+        String floor = in.nextLine();
+        try {
+            int flr = Integer.parseInt(floor);
+            if(flr >= 1 && flr < MAX_FLOOR) {
+                int distance;
+                if(flr > currentFloor) {
+                    distance = flr - currentFloor;
+                    System.out.print("\u001B[33m\nGoing up.. ");
+                    for(int i = 1; i <= distance; i++) {
+                        currentFloor++;
+                        System.out.print(currentFloor + ".. ");
+                    }
+                } else {
+                    distance = currentFloor - flr;
+                    System.out.print("\u001B[33m\nGoing down..");
+                    for(int i = 1; i <= distance; i++) {
+                        currentFloor--;
+                        System.out.print(currentFloor + ".. ");
+                    }
+                }
+                System.out.println("Ding!");
+            } else {
+                System.out.println("\u001B[31mInvalid floor selection, must be between 1 & 100");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("\u001B[31mInvalid floor selection, must be between 1 & 100");
+        }
+    }
+
+    /**
+     * Pulls the fire alarm and forces the elevator to the first floor
+     */
+
+    public void fireAlarm() {
+        System.out.println("\u001B[31mDanger! You must exit the building now!");
+        currentFloor = 1;
+    }
+}
---
+++ src/weekeight/ElevatorDriver.java
@@ -0,0 +1,46 @@
+/*
+ * Course: CS1011 - Saikath Bhattacharya
+ * Fall 2022
+ * Homework 8 - Elevator
+ * Name: Adam Haile
+ * Created: 10/27/22
+ */
+
+package weekeight;
+
+import java.util.Scanner;
+
+/**
+ * The ElevatorDriver - Main file that runs the program
+ */
+
+public class ElevatorDriver {
+    public static void main(String[] args) {
+        boolean run = true;
+        System.out.println("\u001B[36mWelcome to Adam's Elevator Simulator!");
+        Scanner in = new Scanner(System.in);
+        Elevator elevator = new Elevator();
+        while(run) {
+            System.out.println("\u001B[33mOptions: (s)elect a floor, (f)ire alarm, (q)uit" +
+                    "\nEnter s, f, or q ==>");
+            String sel = in.nextLine();
+            switch (sel) {
+                case "s":
+                    System.out.println("\u001B[32mPlease enter the floor you'd like to go to " +
+                            "(1-100) ==>");
+                    elevator.setFloor(in);
+                    break;
+                case "f":
+                    elevator.fireAlarm();
+                    break;
+                case "q":
+                    run = false;
+                    break;
+                default:
+                    System.out.println("\u001B[31mInvalid selection, please choose " +
+                            "between s, f, or q");
+                    break;
+            }
+        }
+    }
+}
